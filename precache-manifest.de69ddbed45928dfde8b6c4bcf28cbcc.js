ccffe6a4b133202e01dff73803f2a0a1833cd49f
Umar Taufiqulhakim <umartaufiqul@gmail.com>
2020-08-20 11:43:37 +0700

    Updates

---
+++ precache-manifest.de69ddbed45928dfde8b6c4bcf28cbcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d671cb638e808c958c3aedef4b15be",
+    "url": "/bias-awareness-platform/index.html"
+  },
+  {
+    "revision": "e959ffd177e5ef043d97",
+    "url": "/bias-awareness-platform/static/css/main.fc789131.chunk.css"
+  },
+  {
+    "revision": "4c2e2d97a90e5876ac29",
+    "url": "/bias-awareness-platform/static/js/2.7dc0386e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bias-awareness-platform/static/js/2.7dc0386e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e959ffd177e5ef043d97",
+    "url": "/bias-awareness-platform/static/js/main.3951a590.chunk.js"
+  },
+  {
+    "revision": "2cc5142573b1b2e59997",
+    "url": "/bias-awareness-platform/static/js/runtime-main.b7e6e405.js"
+  },
+  {
+    "revision": "2a2e6249540a8d71ea2a182aedf9bdcb",
+    "url": "/bias-awareness-platform/static/media/graph.2a2e6249.png"
+  }
+]);
