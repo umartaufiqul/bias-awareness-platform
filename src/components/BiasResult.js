f2669981be37cf0de0bc1f6c06211860b9526136
Umar Taufiqulhakim <umartaufiqul@gmail.com>
2020-09-15 11:53:21 +0700

    Add a way to explain how each bias measure is calculated

--- src/components/BiasResult.js
+++ src/components/BiasResult.js
@@ -1,20 +1,80 @@
 import React, {useState, useEffect} from "react"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
 
 const BiasResult = (props) => {
 
     //The biasStat can be passed through props.biasStat. I just use the accStat one as an example
     const biasStat = props.biasStat
     const [resultAvailable, setResultAvailable] = useState(true)
+    const [measureExplain, setMeasureExplain] = useState("")
 
     //The bias measures here are just sample
     const reportTable = <table className='table' >
         <thead>
             <tr>
                 <th scope="col" className="align-middle">Class</th>
-                <th scope="col" className="align-middle">Disparate Impact</th>
-                <th scope="col" className="align-middle">Statistical Parity Diff.</th>
-                <th scope="col" className="align-middle">Equal Opportunity Diff.</th>
-                <th scope="col" className="align-middle">Average Odds Diff.</th>
+                <th scope="col" className="align-middle">
+                    Disparate Impact 
+                    <div>
+                    <OverlayTrigger
+                        key={'right'}
+                        placement={'right'}
+                        overlay={
+                            <Tooltip id={`tooltip-${'right'}`} className='text-align-left'>
+                                Click here to see how the measure is calculated
+                            </Tooltip>
+                        }
+                        >
+                        <img src={require('../icons/info.svg')} alt='' style={{cursor: "pointer"}} onClick={() => setMeasureExplain("Disparate Impact")}/>
+                    </OverlayTrigger>
+                    </div>
+                </th>
+                <th scope="col" className="align-middle">Statistical Parity Diff.
+                    <div>
+                    <OverlayTrigger
+                        key={'right'}
+                        placement={'right'}
+                        overlay={
+                            <Tooltip id={`tooltip-${'right'}`} className='text-align-left'>
+                                Click here to see how the measure is calculated
+                            </Tooltip>
+                        }
+                        >
+                        <img src={require('../icons/info.svg')} alt='' style={{cursor: "pointer"}} onClick={() => setMeasureExplain("Statictical Parity Diff")}/>
+                    </OverlayTrigger>
+                    </div>
+                </th>
+                <th scope="col" className="align-middle">Equal Opportunity Diff.
+                    <div>
+                    <OverlayTrigger
+                        key={'right'}
+                        placement={'right'}
+                        overlay={
+                            <Tooltip id={`tooltip-${'right'}`} className='text-align-left'>
+                                Click here to see how the measure is calculated
+                            </Tooltip>
+                        }
+                        >
+                        <img src={require('../icons/info.svg')} alt='' style={{cursor: "pointer"}} onClick={() => setMeasureExplain("Equal Opportunity Difference")}/>
+                    </OverlayTrigger>
+                    </div>
+                </th>
+                <th scope="col" className="align-middle">Average Odds Diff.
+                    <div>
+                    <OverlayTrigger
+                        key={'right'}
+                        placement={'right'}
+                        overlay={
+                            <Tooltip id={`tooltip-${'right'}`} className='text-align-left'>
+                                Click here to see how the measure is calculated
+                            </Tooltip>
+                        }
+                        >
+                        <img src={require('../icons/info.svg')} alt='' style={{cursor: "pointer"}} onClick={() => setMeasureExplain("Average Odds Diff")}/>
+                    </OverlayTrigger>
+                    </div>
+                </th>
             </tr>
         </thead>
         <tbody>
@@ -30,12 +90,27 @@ const BiasResult = (props) => {
         </tbody>
     </table>
 
+    //Add the measure explanation here
+    function returnMeasureExplain() {
+        switch(measureExplain){
+            default:
+                return (
+                <div style={{marginTop: "1rem"}}> This is the explanation of how measure is calculated</div>
+                )
+        }
+    }
+
     if (resultAvailable) {
         return(
             <div style={{marginTop: "1rem"}}>
                 <div id='result-table' style={{overflow: "auto", height: "inherit"}}>
                     {/* {activeResult === "report" ? reportTable : distrTable} */}
                     {reportTable}
+                </div>
+                <div className={measureExplain === "" ? "d-none": ""}>
+                    <h5> How the measure is calculated:</h5>
+                    <h6 style={{textDecoration: "underline"}}> {measureExplain} </h6>
+                    {returnMeasureExplain()}
                 </div>
             </div>
         )
